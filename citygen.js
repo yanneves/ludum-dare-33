cd3035b67e99e7df979dc4ec8a01a713f5cbe5bb
aaunel <yann@aaunel.com>
2015-08-22 16:27:08 +0100

    Fixed citygen script

--- citygen.js
+++ citygen.js
@@ -2,6 +2,20 @@ pc.script.attribute('houses', 'number', 2)
 
 pc.script.create('citygen', function (context) {
 
-    console.dir(context)
+    var Citygen = function (entity) {
+        // self-reference
+        this.entity = entity
+    }
 
+    Citygen.prototype = {
+        initialize: function () {
+            console.log(this.houses)
+        },
+
+        update: function (dt) {
+
+        }
+    }
+
+    return Citygen
 })
