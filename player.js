95f03216fb94a6f71ade11899b7a5aeca467c88a
aaunel <yann@aaunel.com>
2015-08-22 14:07:46 +0100

    Corrected torque calculations in player script

--- player.js
+++ player.js
@@ -18,11 +18,11 @@ pc.script.create('player', function (context) {
 
         update: function (dt) {
             if (context.keyboard.isPressed(pc.input.KEY_A)) {
-                this.entity.rigidbody.applyTorqueImpulse(0, 0, -this.torque)
+                this.entity.rigidbody.applyTorqueImpulse(0, 0, this.torque)
             }
 
             if (context.keyboard.isPressed(pc.input.KEY_D)) {
-                this.entity.rigidbody.applyTorqueImpulse(0, 0, this.torque)
+                this.entity.rigidbody.applyTorqueImpulse(0, 0, -this.torque)
             }
 
             if (context.keyboard.isPressed(pc.input.KEY_SPACE)) {
