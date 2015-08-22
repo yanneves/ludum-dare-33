777b315a7ba8c58039a5c705672b1e80f3396a36
aaunel <yann@aaunel.com>
2015-08-22 15:20:43 +0100

    Added player start coords, created reset function

--- player.js
+++ player.js
@@ -1,3 +1,7 @@
+pc.script.attribute('PLAYER_START_X', 'number', 0)
+pc.script.attribute('PLAYER_START_Y', 'number', 10)
+pc.script.attribute('PLAYER_START_Z', 'number', 0)
+
 pc.script.attribute('thrust', 'number', 350)
 pc.script.attribute('torque', 'number', 40)
 
@@ -13,10 +17,12 @@ pc.script.create('player', function (context) {
 
     Player.prototype = {
         initialize: function () {
-
+            // start at reset position
+            // this.reset()
         },
 
         update: function (dt) {
+            // rotation control
             if (context.keyboard.isPressed(pc.input.KEY_A)) {
                 this.entity.rigidbody.applyTorqueImpulse(0, 0, this.torque)
             }
@@ -25,10 +31,25 @@ pc.script.create('player', function (context) {
                 this.entity.rigidbody.applyTorqueImpulse(0, 0, -this.torque)
             }
 
+            // thrust control
             if (context.keyboard.isPressed(pc.input.KEY_SPACE)) {
                 this.thrustVec.copy(this.entity.up).scale(this.thrust)
                 this.entity.rigidbody.applyImpulse(this.thrustVec)
             }
+
+            // player rest
+            if (context.keyboard.isPressed(pc.input.KEY_ENTER)) {
+                this.reset()
+            }
+        },
+
+        reset: function () {
+            this.entity.setPosition(this.PLAYER_START_X, this.PLAYER_START_Y, this.PLAYER_START_Z)
+            this.entity.setEulerAngles(0, 0, 0)
+
+            this.entity.rigidbody.syncEntityToBody()
+            this.entity.rigidbody.linearVelocity = pc.Vec3.ZERO
+            this.entity.rigidbody.angularVelocity = pc.Vec3.ZERO
         }
     }
 
