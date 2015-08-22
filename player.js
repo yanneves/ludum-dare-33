a925b4cb20b52ffe1729d65f92d226b5f62526bf
aaunel <yann@aaunel.com>
2015-08-22 13:32:02 +0100

    Added some basic comments, dropped semicolons

--- player.js
+++ player.js
@@ -1,15 +1,15 @@
-pc.script.attribute('thrust', 'number', 350);
-pc.script.attribute('torque', 'number', 40);
+pc.script.attribute('thrust', 'number', 350)
+pc.script.attribute('torque', 'number', 40)
 
 pc.script.create('player', function (context) {
 
     var Player = function (entity) {
         // self-reference
-        this.entity = entity;
+        this.entity = entity
 
         // vectors
-        this.thrustVec = new pc.Vec3();
-    };
+        this.thrustVec = new pc.Vec3()
+    }
 
     Player.prototype = {
         initialize: function () {
@@ -18,19 +18,19 @@ pc.script.create('player', function (context) {
 
         update: function (dt) {
             if (context.keyboard.isPressed(pc.input.KEY_A)) {
-                this.entity.rigidbody.applyTorqueImpulse(0, 0, -this.torque);
+                this.entity.rigidbody.applyTorqueImpulse(0, 0, -this.torque)
             }
 
             if (context.keyboard.isPressed(pc.input.KEY_D)) {
-                this.entity.rigidbody.applyTorqueImpulse(0, 0, this.torque);
+                this.entity.rigidbody.applyTorqueImpulse(0, 0, this.torque)
             }
 
             if (context.keyboard.isPressed(pc.input.KEY_SPACE)) {
-                this.thrustVec.copy(this.entity.up).scale(this.thrust);
-                this.entity.rigidbody.applyImpulse(this.thrust);
+                this.thrustVec.copy(this.entity.up).scale(this.thrust)
+                this.entity.rigidbody.applyImpulse(this.thrust)
             }
         }
-    };
+    }
 
-    return Player;
-});
+    return Player
+})
